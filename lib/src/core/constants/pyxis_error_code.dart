dceb82b4691177a0f468c50d00faa08f2b0ab8a3
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-01-29 09:15:13 +0700

    #define some exception code

---
+++ lib/src/core/constants/pyxis_error_code.dart
@@ -0,0 +1,13 @@
+sealed class PyxisErrorCode{
+  static const String networkException = '50';
+  static const String sendTimeOut = '51';
+  static const String badCertificate = '52';
+  static const String badResponse = '53';
+  static const String cancel = '54';
+  static const String connectionError = '55';
+  static const String connectionTimeout = '56';
+  static const String receiveTimeout = '57';
+  static const String formatException = '58';
+  static const String unMatchType = '59';
+  static const String unknownError = '99';
+}
--- lib/src/core/exception/pyxis_exception_wrapper.dart
+++ lib/src/core/exception/pyxis_exception_wrapper.dart
@@ -1,23 +1,118 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:domain/domain.dart';
+import 'package:pyxis_mobile/src/core/constants/pyxis_error_code.dart';
 import 'package:pyxis_mobile/src/core/exception/core/base_exeption_wrapper_helper.dart';
 
 final class PyxisExceptionWrapper extends BaseExceptionWrapperHelper {
+  static BaseExceptionWrapperHelper _getInstance() => PyxisExceptionWrapper._instance();
+
+  static BaseExceptionWrapperHelper get instance => _getInstance();
+
+  PyxisExceptionWrapper._instance();
+
   @override
   Object? errorMapperHandler(AppError error) {
-    // TODO: implement errorMapperHandler
-    throw UnimplementedError();
+    return null;
   }
 
   @override
   AppError handleError(Object e) {
     if (e is DioException) {
+      switch (e.type) {
+        case DioExceptionType.sendTimeout:
+          return AppError(
+            code: PyxisErrorCode.sendTimeOut,
+            message: e.message,
+          );
+        case DioExceptionType.badCertificate:
+          return AppError(
+            code: PyxisErrorCode.badCertificate,
+            message: e.message,
+          );
+        case DioExceptionType.badResponse:
+          final response = e.response;
+          return AppError(
+            code: response?.statusCode.toString() ?? PyxisErrorCode.badResponse,
+            message: e.message,
+          );
+        case DioExceptionType.cancel:
+          return AppError(
+            code: PyxisErrorCode.cancel,
+            message: e.message,
+          );
+        case DioExceptionType.connectionError:
+          return AppError(
+            code: PyxisErrorCode.connectionError,
+            message: e.message,
+          );
+        case DioExceptionType.connectionTimeout:
+          return AppError(
+            code: PyxisErrorCode.connectionTimeout,
+            message: e.message,
+          );
+        case DioExceptionType.receiveTimeout:
+          return AppError(
+            code: PyxisErrorCode.receiveTimeout,
+            message: e.message,
+          );
+        case DioExceptionType.unknown:
+          return _convertOtherExceptionOrElse(e);
+      }
+    }
+    return const AppError(
+      code: PyxisErrorCode.unknownError,
+    );
+  }
+
+  AppError _convertOtherExceptionOrElse(Object ex) {
+    if (ex is DioException && ex.error is FormatException) {
       return AppError(
-        code: e.toString(),
+          code: PyxisErrorCode.formatException, message: ex.message);
+    }
+    if (ex is FormatException) {
+      return AppError(
+        code: PyxisErrorCode.formatException,
+        message: ex.message,
+      );
+    }
+    if (ex is DioException && ex.error is SocketException) {
+      return AppError(
+        code: PyxisErrorCode.networkException,
+        message: (ex.error as SocketException).message,
+      );
+    }
+
+    if (ex is SocketException) {
+      return AppError(
+        code: PyxisErrorCode.networkException,
+        message: ex.message,
+      );
+    }
+
+    if (ex is DioException &&
+        ex.error
+            .toString()
+            .toLowerCase()
+            .contains('is not a subtype of'.toLowerCase())) {
+      return AppError(
+        code: PyxisErrorCode.unMatchType,
+        message: ex.message,
+      );
+    }
+
+    if (ex
+        .toString()
+        .toLowerCase()
+        .contains('is not a subtype of'.toLowerCase())) {
+      return AppError(
+        code: PyxisErrorCode.unMatchType,
+        message: ex.toString(),
       );
     }
     return const AppError(
-      code: '0',
+      code: PyxisErrorCode.unknownError,
     );
   }
 }
