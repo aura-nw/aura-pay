16607913b7e9f16dce16ce9f0c8ec032e282683c
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-07-02 14:29:23 +0700

    #add create passcode screen

--- pyxis_v2/lib/src/presentation/screens/splash/spash_screen.dart
+++ pyxis_v2/lib/src/presentation/screens/splash/spash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pyxis_v2/src/application/global/app_theme/app_theme.dart';
+import 'package:pyxis_v2/src/application/global/localization/localization_manager.dart';
+import 'package:pyxis_v2/src/navigator.dart';
 import 'package:pyxis_v2/src/presentation/widgets/base_screen.dart';
 
 final class SplashScreen extends StatefulWidget {
@@ -8,22 +11,33 @@ final class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with StateFulBaseScreen{
-
+class _SplashScreenState extends State<SplashScreen> with StateFulBaseScreen {
   @override
   void initState() {
     super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        Future.delayed(const Duration(seconds: 2)).then(
+          (value) {
+            AppNavigator.push(RoutePath.getStarted);
+          },
+        );
+      },
+    );
   }
 
   @override
-  Widget child(BuildContext context) {
-    // TODO: implement child
-    throw UnimplementedError();
+  Widget child(BuildContext context,AppTheme appTheme, AppLocalizationManager localization) {
+    return const Center(
+      child: Text('Splash screen'),
+    );
   }
 
   @override
-  Widget wrapBuild(BuildContext context, Widget child) {
-    // TODO: implement wrapBuild
-    throw UnimplementedError();
+  Widget wrapBuild(BuildContext context, Widget child,AppTheme appTheme, AppLocalizationManager localization) {
+    return Scaffold(
+      body: child,
+    );
   }
 }
