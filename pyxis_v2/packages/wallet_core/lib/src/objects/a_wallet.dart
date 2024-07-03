d03480a14628a33bf6a11956d46d5ae0403fad0d
Ho Minh Khoa <khoahm.it@gmail.com>
2024-07-03 07:33:57 +0700

    make private key as object, remove stored privateKey as String

--- pyxis_v2/packages/wallet_core/lib/src/objects/a_wallet.dart
+++ pyxis_v2/packages/wallet_core/lib/src/objects/a_wallet.dart
@@ -1,10 +1,21 @@
+import 'package:convert/convert.dart';
 import 'package:wallet_core/wallet_core.dart';
 
 /// Class to hold wallet information.
 class AWallet {
   final HDWallet? wallet;
   final String address;
-  final String privateKey;
+  final PrivateKey _privateKey;
+  final int coinType;
 
-  AWallet({this.wallet, required this.address, required this.privateKey});
+  AWallet(
+      {this.wallet,
+      required this.address,
+      required PrivateKey privateKey,
+      required this.coinType})
+      : _privateKey = privateKey;
+
+  String get privateKey {
+    return hex.encode(_privateKey.data());
+  }
 }
