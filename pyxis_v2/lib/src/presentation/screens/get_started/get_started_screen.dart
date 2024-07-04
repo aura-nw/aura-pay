a2300c6129539869ba278bcb16c5d8c8014f2900
Nguyen Van Toan <77067868+ToanBarcelona1998@users.noreply.github.com>
2024-07-04 17:07:48 +0700

    #added get started screen

--- pyxis_v2/lib/src/presentation/screens/get_started/get_started_screen.dart
+++ pyxis_v2/lib/src/presentation/screens/get_started/get_started_screen.dart
@@ -1,8 +1,14 @@
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
+import 'package:pyxis_v2/app_configs/di.dart';
+import 'package:pyxis_v2/app_configs/pyxis_mobile_config.dart';
 import 'package:pyxis_v2/src/application/global/app_theme/app_theme.dart';
 import 'package:pyxis_v2/src/application/global/localization/localization_manager.dart';
-import 'package:pyxis_v2/src/core/constants/size_constant.dart';
-import 'package:pyxis_v2/src/presentation/widgets/app_button.dart';
+import 'package:pyxis_v2/src/core/constants/app_local_constant.dart';
+import 'package:pyxis_v2/src/core/utils/context_extension.dart';
+import 'package:pyxis_v2/src/navigator.dart';
+import 'package:pyxis_v2/src/presentation/screens/get_started/widgets/button_form.dart';
+import 'package:pyxis_v2/src/presentation/screens/get_started/widgets/logo_form.dart';
 import 'package:pyxis_v2/src/presentation/widgets/base_screen.dart';
 
 class GetStartedScreen extends StatefulWidget {
@@ -14,36 +20,67 @@ class GetStartedScreen extends StatefulWidget {
 
 class _GetStartedScreenState extends State<GetStartedScreen>
     with StateFulBaseScreen {
+  final PyxisMobileConfig _config = getIt.get<PyxisMobileConfig>();
+
   @override
-  Widget child(BuildContext context, AppTheme appTheme, AppLocalizationManager localization) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          PrimaryAppButton(
-            text: 'Create',
-            onPress: () {
-
-            },
+  Widget child(BuildContext context, AppTheme appTheme,
+      AppLocalizationManager localization) {
+    return Column(
+      children: [
+        Expanded(
+          child: GetStartedLogoFormWidget(
+            walletName: _config.appName,
+            appTheme: appTheme,
           ),
-          const SizedBox(
-            height: BoxSize.boxSize05,
-          ),
-          PrimaryAppButton(
-            text: 'Import',
-            onPress: () {
-
-            },
-          ),
-        ],
-      ),
+        ),
+        GetStartedButtonFormWidget(
+          localization: localization,
+          appTheme: appTheme,
+          onCreateNewWallet: () {
+            _onCheckHasPasscode(_onCreateNew);
+          },
+          onImportExistingWallet: () {
+            _onCheckHasPasscode(_onAddExistingWallet);
+          },
+          onTermClick: () {},
+        ),
+      ],
     );
   }
 
   @override
-  Widget wrapBuild(BuildContext context, Widget child, AppTheme appTheme, AppLocalizationManager localization) {
+  Widget wrapBuild(BuildContext context, Widget child, AppTheme appTheme,
+      AppLocalizationManager localization) {
     return Scaffold(
+      backgroundColor: appTheme.bgPrimary,
       body: child,
     );
   }
+
+  void _onCheckHasPasscode(
+    VoidCallback callBack,
+  ) async {
+    final appSecureUseCase = getIt.get<AppSecureUseCase>();
+
+    final bool hasPassCode = await appSecureUseCase.hasPasscode(
+      key: AppLocalConstant.passCodeKey,
+    );
+
+    if (hasPassCode) {
+      callBack.call();
+    } else {
+      AppNavigator.push(
+        RoutePath.setPasscode,
+        _onCreateNew,
+      );
+    }
+  }
+
+  void _onCreateNew() {
+    AppNavigator.replaceWith(
+      RoutePath.createWallet,
+    );
+  }
+
+  void _onAddExistingWallet() {}
 }
