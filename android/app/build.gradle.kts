c48379d04872915d7b73913b0da589d798dd3f20
KhoaHM <khoahm.it@gmail.com>
2025-10-20 13:23:38 +0700

    migrate version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.aura.network.pay.aurapay"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26  // Updated to support web3auth_flutter plugin
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,18 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Fix namespace not specified error for subprojects
+    afterEvaluate {
+        if (hasProperty("android")) {
+            val android = extensions.getByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                if (android.namespace == null) {
+                    android.namespace = group as String?
+                }
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
